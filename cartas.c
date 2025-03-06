b42d93e88f0f021586ee4d93e46dc337be217032
rikellyfarias <rikellyfarias26@gmail.com>
2025-03-06 22:56:02 +0000

    criei duas cartas

---
+++ cartas.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+ 
+    int carta = 1;
+    char estado = 'A';
+    char codigo[10] = "A01";
+    char cidade[10] = "Alagoas";
+    int população;
+    float área;
+    float PIB;
+    int turismo;
+
+    printf("Carta: %d\nEstado: %c\nCódigo: %s\nCidade: %s\n", carta, estado, codigo, cidade);
+
+
+
+
+
+}
