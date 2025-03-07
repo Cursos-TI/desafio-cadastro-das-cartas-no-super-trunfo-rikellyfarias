f0017d4b8a5ae163fca48cc4acc41b5c929d91ee
rikellyfarias <rikellyfarias26@gmail.com>
2025-03-07 22:55:42 +0000

    criaçãodeduascartas.c

--- cartas.c
+++ cartas.c
@@ -23,9 +23,10 @@ int main() {
     scanf("%f", &PIB);
 
     printf("Número de pontos turísticos: ");
-    scanf("%d", &turismo);
+    scanf(" %c", &turismo);
 
-    printf("\nPopulação: %d\nÁrea: %f km²\nPIB: %f bilhões de reais\nNúmero de Pontos Turísticos: %d\n", populacao, area, PIB, turismo);
+    printf("\nCarta: %d\nEstado: %c\nCódigo da Carta: %s\nCidade: %s\n", carta, estado, codigo, cidade);
+    printf("População: %d\nÁrea: %f km²\nPIB: %f bilhões de reais\nNúmero de Pontos Turísticos: %c\n", populacao, area, PIB, turismo);
 
     int carta2 = 2;
     char estado2 = 'B';
@@ -42,16 +43,18 @@ int main() {
     scanf("%d", &populacao2);
 
     printf("Área: ");
-    scanf("%.2f", &area2);
+    scanf("%f", &area2);
 
     printf("PIB: ");
-    scanf("%.2f", &PIB2);
+    scanf("%f", &PIB2);
 
     printf("Número de pontos turísticos: ");
-    scanf("%d", &turismo2);
+    scanf(" %c", &turismo2);
 
-    printf("\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d", populacao2, area2, PIB2, turismo2);
-    
+    printf("\nCarta %d\nEstado: %c\nCódigo da Carta: %s\nCidade: %s", carta2, estado2, codigo2, cidade);
+    printf("População: %d\nÁrea: %f km²\nPIB: %f bilhões de reais\nNúmero de Pontos Turísticos: %c", populacao2, area2, PIB2, turismo2);
+    //printf irá imprimir as informações que o usúario adicionou no scanf na carta 2
+
     return 0;
 
 }
